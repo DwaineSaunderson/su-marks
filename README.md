eae70783516149fe0e2e53a49eaf8bbafc800fa1
Dwaine Saunderson <19236980@sun.ac.za>
2018-06-12 16:21:34 +0200

    Initial Commit with Existing Sources

---
+++ README.md
@@ -0,0 +1,19 @@
+# SU Mark Scraper
+
+Python Script which automates fetching of marks for Stellenbosch University students. Useful for monitoring changes to your report card during and after exams.
+
+## Getting Started
+
+Clone this repository.
+
+### Prerequisites
+
+Python3, lxml and requests.
+
+## Running The Thing
+
+After cloning the repository:
+```sh
+$ cd ~/su-marks
+$ ./sumarks <Username>
+```
---
+++ sumarks.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+
+import requests
+from lxml import html
+import getpass
+import sys
+
+session_requests = requests.session()
+
+
+def auth_user(username, password):
+    login_url = "https://sso-prod.sun.ac.za/cas/login? \
+    TARGET=http://t2000-05.sun.ac.za/EksamenUitslae/EksUitslae.jsp"
+
+    result = session_requests.get(login_url)
+
+    tree = html.fromstring(result.text)
+    login_token = list(set(tree.xpath("//input[@name='lt']/@value")))[0]
+    execstr = list(set(tree.xpath("//input[@name='execution']/@value")))[0]
+
+    payload = {
+        "username": username,
+        "password": password,
+        "lt": login_token,
+        "execution": execstr,
+        "_eventId": "submit"
+    }
+
+    result = session_requests.post(
+        login_url,
+        data=payload,
+        headers=dict(referer=login_url)
+    )
+
+
+def fetch_marks():
+
+    url = "http://t2000-05.sun.ac.za/EksamenUitslae/EksUitslae.jsp"
+
+    result = session_requests.get(
+        url,
+        headers=dict(referer=url)
+    )
+
+    tree = html.fromstring(result.text)
+    table = tree.xpath("//table[2]")
+
+    result = {}
+
+    for i in range(2, len(table[0])):
+        modname = table[0][i][1].text_content()
+        modname = ' '.join(modname.split())
+        modmark = table[0][i][5].text_content()
+        modmark = ' '.join(modmark.split())
+        result[modname] = modmark
+    return result
+
+
+def print_results(results_dict):
+    for key, value in results_dict.items():
+        print("{0} - {1}".format(key, value))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} <Username>".format(sys.argv[0]))
+        exit()
+    username = sys.argv[1]
+    password = getpass.getpass()
+    auth_user(username, password)
+    try:
+        result = fetch_marks()
+        print_results(result)
+    except(IndexError):
+        print("Parsing Error (Check Password?)")
+        exit(1)
